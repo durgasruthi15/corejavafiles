a098893b3e5df6bc5655405705d9f9b9738e4d71
durgasruthi15 <durgasruthi15@gmail.com>
2024-02-08 22:51:33 -0600

    maps

---
+++ src/main/java/functionalinterface/CaseConverter.java
@@ -0,0 +1,17 @@
+package functionalinterface;
+import java.lang.Character;
+public class CaseConverter {
+    public String convertCase(String input) {
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            }
+        }
+        return new String(chars);
+    }
+}
+
